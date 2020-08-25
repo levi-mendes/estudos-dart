d42c15d809280a408a28cd1a69759151ae794a03
Levi Mendes <levi.mendes@foursys.com.br>
2020-08-25 11:47:21 -0300

    funcionalidades basicas classe ContaCorrente

--- estudosdartalura/bin/estudosdartalura.dart
+++ estudosdartalura/bin/estudosdartalura.dart
@@ -1,18 +1,43 @@
 void main() {
   var conta = ContaCorrente();
   conta.titular = 'Levi';
+  conta.agencia = 3282;
+  conta.conta = 301;
 
-  print(conta.toString());
+  print('Saldo = ${conta.getSaldo()}');
+  print('Deposito = ${conta.deposito(500)}');
+  print('Saque = ${conta.saque(200)}');
+  print('Saldo = ${conta.getSaldo()}');
 }
 
 class ContaCorrente {
   String titular;
   int agencia;
   int conta;
-  double saldo;
+  double _saldo = 50.0;
+
+  bool saque(double valor) {
+    if (!temLimite(valor)) {
+      print('Limite insuficiente');
+      return false;
+    }
+
+    _saldo -= valor;
+    //print('Valor sacado = $valor');
+    return true;
+  }
+
+  double getSaldo() => _saldo;
+
+  double deposito(double valor) {
+    _saldo += valor;
+    return _saldo;
+  }
+
+  bool temLimite(double valor) => _saldo > valor;
 
   @override
   String toString() {
-    return 'Titular $titular \nAgencia $agencia \nConta $conta \nSaldo $saldo';
+    return 'Titular $titular \nAgencia $agencia \nConta $conta \nSaldo $_saldo';
   }
-} //le vi
+}
