cf6bb7abb204053e0e26d4c7eec00c65e47c2c5f
Levi Mendes <levi.mendes@foursys.com.br>
2020-08-25 08:38:59 -0300

    commit threee levi

--- estudosdartalura/bin/estudosdartalura.dart
+++ estudosdartalura/bin/estudosdartalura.dart
@@ -1,8 +1,18 @@
-void main() {}
+void main() {
+  var conta = ContaCorrente();
+  conta.titular = 'Levi';
+
+  print(conta.toString());
+}
 
 class ContaCorrente {
   String titular;
   int agencia;
   int conta;
   double saldo;
+
+  @override
+  String toString() {
+    return 'Titular $titular \nAgencia $agencia \nConta $conta \nSaldo $saldo';
+  }
 }
