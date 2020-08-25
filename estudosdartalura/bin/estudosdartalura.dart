c229a25d5d4e7bbb0ccda4b7f65e1ca353e83bde
Levi Mendes <levi.mendes@foursys.com.br>
2020-08-25 12:11:08 -0300

    implementacao do metodo de transferencia

--- estudosdartalura/bin/estudosdartalura.dart
+++ estudosdartalura/bin/estudosdartalura.dart
@@ -3,11 +3,21 @@ void main() {
   conta.titular = 'Levi';
   conta.agencia = 3282;
   conta.conta = 301;
+  conta.deposito(10000.0);
 
-  print('Saldo = ${conta.getSaldo()}');
+  /*print('Saldo = ${conta.getSaldo()}');
   print('Deposito = ${conta.deposito(500)}');
   print('Saque = ${conta.saque(200)}');
-  print('Saldo = ${conta.getSaldo()}');
+  print('Saldo = ${conta.getSaldo()}');*/
+
+  print('Saldo conta Origem ( Antes ) = ${conta.getSaldo()}');
+
+  var contaDestino = ContaCorrente();
+  print('Saldo conta Destino ( Antes ) = ${contaDestino.getSaldo()}');
+  conta.transferencia(35.0, contaDestino);
+  print('Saldo conta Destino ( Depois ) = ${contaDestino.getSaldo()}');
+
+  print('Saldo conta Origem ( Depois ) = ${conta.getSaldo()}');
 }
 
 class ContaCorrente {
@@ -23,7 +33,17 @@ class ContaCorrente {
     }
 
     _saldo -= valor;
-    //print('Valor sacado = $valor');
+    return true;
+  }
+
+  bool transferencia(double valor, ContaCorrente contaDestino) {
+    if (!temLimite(valor)) {
+      print('Saldo Insuficiente !');
+      return false;
+    }
+
+    _saldo -= valor;
+    contaDestino.deposito(valor);
     return true;
   }
 
