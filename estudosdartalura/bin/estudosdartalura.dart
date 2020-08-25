b6046828f4abb769ed5b12b3e6ad94284b6346ff
Levi Mendes <levi.mendes@foursys.com.br>
2020-08-25 14:55:24 -0300

    Refactoring ( packages )

--- estudosdartalura/bin/estudosdartalura.dart
+++ estudosdartalura/bin/estudosdartalura.dart
@@ -1,3 +1,6 @@
+import 'package:estudosdartalura/ContaCorrente.dart';
+import 'package:estudosdartalura/Cliente.dart';
+
 void main() {
   /*var conta = ContaCorrente();
   conta.titular = 'Levi';
@@ -31,53 +34,4 @@ void main() {
   contaLevi.transferencia(61.0, contaRafaela);
   print('Saldo conta Destino ( Depois ) = ${contaRafaela.getSaldo()}');
   print('Saldo conta Origem ( Depois ) = ${contaLevi.getSaldo()}');
-}
-
-class ContaCorrente {
-  Cliente titular;
-  int agencia;
-  int conta;
-  double _saldo = 50.0;
-
-  bool saque(double valor) {
-    if (!temLimite(valor)) {
-      print('Limite insuficiente');
-      return false;
-    }
-
-    _saldo -= valor;
-    return true;
-  }
-
-  bool transferencia(double valor, ContaCorrente contaDestino) {
-    if (!temLimite(valor)) {
-      print('\n---------- Saldo Insuficiente !!!!! -----------\n');
-      return false;
-    }
-
-    _saldo -= valor;
-    contaDestino.deposito(valor);
-    return true;
-  }
-
-  double valorChequeEspecial() => -10.0;
-
-  double getSaldo() => _saldo;
-
-  double deposito(double valor) {
-    _saldo += valor;
-    return _saldo;
-  }
-
-  bool temLimite(double valor) => _saldo >= (valor + valorChequeEspecial());
-
-  @override
-  String toString() =>
-      'Titular $titular \nAgencia $agencia \nConta $conta \nSaldo $_saldo';
-}
-
-class Cliente {
-  String nome;
-  String cpf;
-  String profissao;
 }
